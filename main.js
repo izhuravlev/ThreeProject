8e0156bd51598a42826cb2edd57ed5c5252f9c7f
izhur <ilya.zhurik@gmail.com>
2024-08-30 12:41:28 -0400

    Starter project

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
 import * as THREE from "three";
+
+console.log("JS is working!");
+
+// Sizes
+const sizes = {
+  width: 800,
+  height: 600,
+};
+
+// Canvas
+const canvas = document.querySelector("canvas.webgl");
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+// Camera
+const camera = new THREE.PerspectiveCamera(90, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+
+renderer.render(scene, camera);
